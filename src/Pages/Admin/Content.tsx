33ede69b857edfe341d1be3d69fcde7c28db961f
Shayne Thiessen <shayne@shaynethiessen.com>
2023-12-14 07:38:31 -0600

    feat: get user details

--- src/Pages/Admin/Content.tsx
+++ src/Pages/Admin/Content.tsx
@@ -1,9 +1,19 @@
 import {Text, View} from "react-native";
+import {Button} from "react-native-paper";
+import {server} from "../../core/server";
 
 export function Content() {
+
+    function runMigration() {
+        server
+            .fetch('migrations/run', {}, undefined, 'PUT').then(() => {
+                console.log('run migrations');
+        }).catch(error => console.log(error));
+    }
+
     return (
         <View>
-            <Text>Admin</Text>
+            <Button onPress={runMigration}>Run Migration</Button>
         </View>
     )
 }
--- src/Pages/Home/Content.tsx
+++ src/Pages/Home/Content.tsx
@@ -1,11 +1,21 @@
 import debug from 'debug';
 import {StyleSheet, Text, View} from 'react-native';
+import {server} from "../../core/server";
+import {useEffect, useState} from "react";
 
 const d = debug('terrene.server');
 
 export function Content() {
+	const [users, setUsers] = useState<[]>();
+	useEffect(() => {
+		server
+			.fetch('users/get-top-users', {}, undefined, 'POST').then((response) => {
+			setUsers(response.data);
+		}).catch(error => console.log(error));
+	});
 	return (
 		<View>
+			{users?.map(user => <Text key={user.id}>{JSON.stringify(user)}</Text>)}
 			<Text>Welcome to Site Seeker</Text>
 		</View>
 	);
